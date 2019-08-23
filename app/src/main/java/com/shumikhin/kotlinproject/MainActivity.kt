f78297e46a2c2fcf005cf5e68326a62d92a6a1cf
shumvlesu <sashumihin@gmail.com>
2019-08-23 15:10:55 +0300

    Котика добавил

--- app/src/main/java/com/shumikhin/kotlinproject/MainActivity.kt
+++ app/src/main/java/com/shumikhin/kotlinproject/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.shumikhin.kotlinproject
 
+import android.animation.ObjectAnimator
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -23,6 +26,16 @@ class MainActivity : AppCompatActivity() {
         // Convert value to a number and increment it
         var count: Int = Integer.parseInt(countString)
         count++
+        if (count==100) {
+
+            val animation = AnimationUtils.loadAnimation(this,R.anim.cat1)
+            imageCat.startAnimation(animation)
+
+
+           // val objectAnimator = ObjectAnimator.ofFloat(ImageView, "translationX", 100f)
+            //objectAnimator.duration = 1000
+            //objectAnimator.start()
+        }
         // Display the new value in the text view.
         textView.text = count.toString()
     }
